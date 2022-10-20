4505c68c0adaffca530ebd1b8074bd68ce7dbe4d
flportilla <flportilla@gmail.com>
2022-10-20 12:02:06 -0500

    Finish the app

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountryServiceService } from '../../services/country-service.service';
+import { CountrySmall } from '../../interfaces/countries.interface';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-selector-page',
@@ -8,9 +12,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectorPageComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup = this.fb.group({
+    region: ['', [Validators.required]],
+    country: ['', [Validators.required]],
+    border: ['', [Validators.required]],
+  })
+
+  //fill selectors
+  regions: string[] = []
+  countries: CountrySmall[] = []
+  borders: string[] | null = []
+
+  //loading UI
+  loading: boolean = false
+
+  constructor(
+    private fb: FormBuilder,
+    private countryService: CountryServiceService
+  ) { }
 
   ngOnInit(): void {
+
+    this.regions = this.countryService.regions
+
+    //on region change
+    this.myForm.get('region')?.valueChanges
+      .pipe(
+        tap(() => {
+          this.myForm.get('country')?.reset('')
+          this.loading = true
+        }),
+        switchMap(region => this.countryService.getCountriesByRegion(region))
+      )
+      .subscribe(countries => {
+        this.countries = countries
+        this.loading = false
+      })
+
+    //on country change
+    this.myForm.get('country')?.valueChanges
+      .pipe(
+        tap(() => {
+          this.myForm.get('border')?.reset('')
+          this.loading = true
+        }),
+        switchMap(code => this.countryService.getCountryByCode(code))
+      )
+      .subscribe(country => {
+          this.borders = country[0]?.borders || []
+          this.loading = false
+
+      })
+  }
+
+  save() {
+    console.log(this.myForm.value);
   }
 
 }
--- src/app/countries/services/country-service.service.ts
+++ src/app/countries/services/country-service.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CountryResponse, CountrySmall } from '../interfaces/countries.interface';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryServiceService {
 
-  constructor() { }
+  private _regions: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
+  private _baseUrl: string = 'https://restcountries.com/v2'
+  get regions(): string[] {
+    return [...this._regions];
+  }
+
+  constructor(private http: HttpClient) { }
+
+  getCountriesByRegion(region: string): Observable<CountrySmall[]> {
+    const url: string = `${this._baseUrl}/region/${region}?fields=alpha3Code,name`
+    return this.http.get<CountrySmall[]>(url);
+  }
+
+  getCountryByCode(code: string): Observable<CountryResponse[] | []> {
+
+    if (!code) return of([])
+
+    const url: string = `${this._baseUrl}/alpha?codes=${code}`
+    return this.http.get<CountryResponse[]>(url)
+  }
+  getCountryByCodeSmall(code: string): Observable<CountrySmall> {
+
+    const url: string = `${this._baseUrl}/alpha?codes=${code}?fields=alpha3Code,name`
+    return this.http.get<CountrySmall>(url)
+  }
+
+  
+
 }
